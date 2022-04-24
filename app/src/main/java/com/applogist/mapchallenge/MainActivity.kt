64f672fa20275d96334c707df7f07c7dd6734f3c
Murat Güleçyüz <murat.gulecyuz@applogist.com>
2022-04-24 04:56:12 +0300

    google map integrated.

--- app/src/main/java/com/applogist/mapchallenge/MainActivity.kt
+++ app/src/main/java/com/applogist/mapchallenge/MainActivity.kt
@@ -1,13 +1,40 @@
 package com.applogist.mapchallenge
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.fragment.NavHostFragment
 import com.applogist.mapchallenge.base.BaseActivity
 import com.applogist.mapchallenge.databinding.ActivityMainBinding
 
-class MainActivity : BaseActivity<ActivityMainBinding>() {
+class MainActivity : BaseActivity<ActivityMainBinding>(),
+    NavController.OnDestinationChangedListener {
+    private lateinit var navController: NavController
+
     override fun getLayoutId(): Int {
         return R.layout.activity_main
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+       // initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.navHostFragment
+        ) as NavHostFragment
+        navController = navHostFragment.navController
+
+        navController.addOnDestinationChangedListener(this)
+    }
+
+    override fun onDestinationChanged(
+        controller: NavController,
+        destination: NavDestination,
+        arguments: Bundle?
+    ) {
+
     }
 
 }
--- app/src/main/java/com/applogist/mapchallenge/ui/map/MapFragment.kt
+++ app/src/main/java/com/applogist/mapchallenge/ui/map/MapFragment.kt
@@ -1,9 +1,33 @@
 package com.applogist.mapchallenge.ui.map
 
+import android.os.Bundle
+import android.view.View
 import com.applogist.mapchallenge.R
 import com.applogist.mapchallenge.base.BaseFragment
 import com.applogist.mapchallenge.databinding.FragmentMapBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MapFragment : BaseFragment<FragmentMapBinding>() {
+class MapFragment : BaseFragment<FragmentMapBinding>(), OnMapReadyCallback {
+    private lateinit var mMap: GoogleMap
+
     override fun getLayoutId(): Int = R.layout.fragment_map
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
 }
